6da0f76068b094d6cd9a0b5f4175ba19ef596ddb
PRASADHAJARgw <prasadhajare2000@gamil.com>
2025-05-18 01:56:55 +0530

    final sheet update

--- google_sheets_handler.py
+++ google_sheets_handler.py
@@ -2,19 +2,31 @@ import gspread
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+# def init_gsheets():
+#     scope = [
+#         "https://spreadsheets.google.com/feeds",
+#         "https://www.googleapis.com/auth/drive"
+#     ]
+#     secrets_path = os.path.join(os.getcwd(), ".streamlit", "secrets.toml")
+#     if os.path.exists(secrets_path):
+#         if "GOOGLE_CREDS" in st.secrets and st.secrets["GOOGLE_CREDS"]:
+#             creds_dict = json.loads(st.secrets["GOOGLE_CREDS"])
+#             creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+#         else:
+#             raise RuntimeError("GOOGLE_CREDS not found in Streamlit secrets.")
+#     else:  # Local development
+#         creds = ServiceAccountCredentials.from_json_keyfile_name("config/creds.json", scope)
+#     client = gspread.authorize(creds)
+#     return client
 def init_gsheets():
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    secrets_path = os.path.join(os.getcwd(), ".streamlit", "secrets.toml")
-    if os.path.exists(secrets_path):
-        if "GOOGLE_CREDS" in st.secrets and st.secrets["GOOGLE_CREDS"]:
-            creds_dict = json.loads(st.secrets["GOOGLE_CREDS"])
-            creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-        else:
-            raise RuntimeError("GOOGLE_CREDS not found in Streamlit secrets.")
-    else:  # Local development
+    if "GOOGLE_CREDS" in st.secrets:
+        creds_dict = dict(st.secrets["GOOGLE_CREDS"])
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    else:
         creds = ServiceAccountCredentials.from_json_keyfile_name("config/creds.json", scope)
     client = gspread.authorize(creds)
     return client
